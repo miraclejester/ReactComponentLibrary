03db995d3d2f1d8b5b2843f6542c709acf331516
miraclejester <miraclejester413@gmail.com>
2026-07-28 12:33:23 -0400

    Badge

---
+++ components/Badge/Badge.jsx
@@ -0,0 +1,16 @@
+﻿import classnames from "classnames";
+import './badge.css';
+
+export default function Badge({ children, variant = 'square', color = 'gray' }) {
+    const variantClass = variant ? `badge-${variant}` : '';
+    const colorClass = color ? `badge-${color}` : '';
+    const allClasses = classnames('badge', variantClass, colorClass);
+    
+    console.log(allClasses);
+    
+    return (
+        <div className={allClasses}>
+            {children}
+        </div>
+    )
+}
--- index.jsx
+++ index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Button from "./components/Button/Button";
+import Badge from "./components/Badge/Badge";
 
 function App() {
   return (
-      <Button size="lg" variant="success">Sample</Button>
+      <Badge variant='pill' color='yellow'>Badge</Badge>
   )
 }
 
